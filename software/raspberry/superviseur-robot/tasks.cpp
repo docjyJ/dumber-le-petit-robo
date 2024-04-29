7f01725899f47c4fc02d7259a68fb16b4e218137
Jean-Yves <7360784+docjyJ@users.noreply.github.com>
2024-04-29 09:51:07 +0200

    tibo il jou avec sont robo

--- software/raspberry/superviseur-robot/tasks.cpp
+++ software/raspberry/superviseur-robot/tasks.cpp
@@ -252,21 +252,21 @@ void Tasks::Run() {
  * @brief Arrêt des tâches
  */
 void Tasks::Stop() {
-    rt_mutex_acquire(&mutex_cameraStarted, TM_INFINITE);
-    cameraStarted = 0;
-    rt_mutex_release(&mutex_cameraStarted);
-    rt_mutex_acquire(&mutex_camera, TM_INFINITE);
-    camera->Close();
-    rt_mutex_release(&mutex_camera);
-    rt_mutex_acquire(&mutex_robotStarted, TM_INFINITE);
-    robotStarted = 0;
-    rt_mutex_release(&mutex_robotStarted);
     rt_mutex_acquire(&mutex_robot, TM_INFINITE);
     robot.Close();
     rt_mutex_release(&mutex_robot);
     rt_mutex_acquire(&mutex_monitor, TM_INFINITE);
     monitor.Close();
     rt_mutex_release(&mutex_monitor);
+    rt_mutex_acquire(&mutex_camera, TM_INFINITE);
+    camera->Close();
+    rt_mutex_release(&mutex_camera);
+    rt_mutex_acquire(&mutex_cameraStarted, TM_INFINITE);
+    cameraStarted = 0;
+    rt_mutex_release(&mutex_cameraStarted);
+    rt_mutex_acquire(&mutex_robotStarted, TM_INFINITE);
+    robotStarted = 0;
+    rt_mutex_release(&mutex_robotStarted);
 
 }
 
